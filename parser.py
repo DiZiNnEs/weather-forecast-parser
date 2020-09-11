c56c8a3d312cf9913fe6991873f270d83ead642b
Iglek Kuat <dizinnes228@gmail.com>
2020-09-11 16:46:13 +0600

    fit: update parser.py and add method for get html

--- parser.py
+++ parser.py
@@ -1,5 +1,18 @@
 import aiohttp
 import asyncio
+from aiohttp.client import ClientTimeout
+
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
+url = 'https://www.gismeteo.kz/weather-kirgizovo-197163/month/'
+
+
+async def request(url: str, headers: str) -> None:
+    session = aiohttp.ClientSession()
+    async with session.get(url=url, headers=headers) as response:
+        print(response.status)
+        print(await response.text())
+
+    await session.close()
 
 
 async def fetch(session, url):
@@ -15,4 +28,4 @@ async def main():
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    loop.run_until_complete(request(url, user_agent))
