c2cd8a4259779024bb036ded63a7472dfd77e7ba
Iglek Kuat <dizinnes228@gmail.com>
2020-09-11 17:03:12 +0600

    fix: change variable user_agent

--- parser.py
+++ parser.py
@@ -1,29 +1,22 @@
+from typing import Set
+
 import aiohttp
 import asyncio
-from aiohttp.client import ClientTimeout
 
-user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
+user_agent = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
+}
 url = 'https://www.gismeteo.kz/weather-kirgizovo-197163/month/'
 
 
-async def request(url: str, headers: str) -> None:
+async def request(url_: str, headers: Set[str]) -> None:
     session = aiohttp.ClientSession()
-    async with session.get(url=url, headers=headers) as response:
+
+    async with session.get(url=url_, headers=headers) as response:
         print(response.status)
         print(await response.text())
 
     await session.close()
-
-
-async def fetch(session, url):
-    async with session.get(url) as response:
-        return await response.text()
-
-
-async def main():
-    async with aiohttp.ClientSession() as session:
-        html = await fetch(session, 'https://www.gismeteo.kz/weather-kirgizovo-197163/month/')
-        print(html)
 
 
 if __name__ == '__main__':
