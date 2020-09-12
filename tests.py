cc540daedf1b88002ec814f956dd3d825dcaae25
Iglek Kuat <dizinnes228@gmail.com>
2020-09-12 16:44:33 +0600

    fix: method name

--- tests.py
+++ tests.py
@@ -1,39 +1,15 @@
-# import unittest
-# import parser
-
-# Sync unittest
-# ---------------------------------------------------------------------------------------------------------------------
-# user_agent = {
-#     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
-# }
-# url = 'https://www.gismeteo.kz/weather-kirgizovo-197163/month/'
-#
-#
-# class MyTestCase(unittest.TestCase):
-#     async def request_test(self):
-#         result = await parser.request(url, user_agent)
-#         self.assertEqual(result, 335873)
-#
-#
-# if __name__ == '__main__':
-#     unittest.main()
-# ---------------------------------------------------------------------------------------------------------------------
-
-import asyncio
-import aiounittest
 import parser
 
-user_agent = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
-}
-url = 'https://www.gismeteo.kz/weather-kirgizovo-197163/month/'
+
+from unittest import main
+from aiounittest import AsyncTestCase
 
 
-class MyTest(aiounittest.AsyncTestCase):
-    async def parser_test(self):
-        result = await parser.request(url, user_agent)
-        self.assertEqual(result, 335879)
+class MyTestCase(AsyncTestCase):
+    async def test_parser(self):
+        result = await parser.request(parser.url, parser.user_agent)
+        self.assertEqual(len(result), 332418)
 
 
 if __name__ == '__main__':
-    aiounittest.AsyncTestCase()
+    main()
