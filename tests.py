4b82d94c7f3218e30659d324c6ecce1e4c719ba3
Iglek Kuat <dizinnes228@gmail.com>
2020-09-12 13:07:20 +0600

    fit: make unittest

--- tests.py
+++ tests.py
@@ -1,9 +1,16 @@
 import unittest
+import parser
+
+user_agent = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
+}
+url = 'https://www.gismeteo.kz/weather-kirgizovo-197163/month/'
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    async def request_test(self):
+        result = await parser.request(url, user_agent)
+        self.assertEqual(result, 335873)
 
 
 if __name__ == '__main__':
