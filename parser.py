d88c27685b303cb83b37a8c5dd52aaf6a2e1abf1
Iglek Kuat <dizinnes228@gmail.com>
2020-09-13 14:17:35 +0600

    refactor: change imports, change indent

--- parser.py
+++ parser.py
@@ -1,9 +1,9 @@
+from aiohttp import ClientSession
+from asyncio import get_event_loop
+
 from bs4 import BeautifulSoup
 
 from typing import Dict
-
-import aiohttp
-import asyncio
 
 user_agent = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
@@ -12,23 +12,20 @@ url = 'https://www.gismeteo.kz/weather-kokshetau-4616/month/'
 
 
 async def request(url_: str, headers: Dict) -> str:
-    session = aiohttp.ClientSession()
-
+    session = ClientSession()
     async with session.get(url=url_, headers=headers) as response:
         content = await response.text()
-
     await session.close()
 
     return content
 
 
 async def html_processing() -> None:
-    print('Hello')
     soup = BeautifulSoup(await request(url, user_agent), 'html.parser')
     for html in soup.find_all('div'):
         print(html)
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
+    loop = get_event_loop()
     loop.run_until_complete(html_processing())
