0d7f4f58776944e0f8638fd8042a5cad5001c7b7
Iglek Kuat <dizinnes228@gmail.com>
2020-09-13 14:22:10 +0600

    refactor: change assert equal in unittest

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ from aiounittest import AsyncTestCase
 class MyTestCase(AsyncTestCase):
     async def test_parser(self):
         result = await parser.request(parser.url, parser.user_agent)
-        self.assertEqual(len(result), 332418)
+        self.assertEqual(True, len(result) > 330000)
 
 
 if __name__ == '__main__':
