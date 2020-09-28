c69a6e9f9234f90323a518931a3ef7ae05368b46
Iglek Kuat <dizinnes228@gmail.com>
2020-09-28 19:10:33 +0600

    tests(root): update tests and add new test

--- tests.py
+++ tests.py
@@ -5,7 +5,13 @@ from aiounittest import AsyncTestCase
 
 class MyTestCase(AsyncTestCase):
 
+    async def test_request(self):
+        print(1)
+        result = await parser.request()
+        self.assertEqual(True, len(result) > 13000)
+
     async def test_parser(self):
+        print(2)
         request = await parser.request()
         result = await parser.response(response_text=request)
         self.assertEqual(True, len(result) > 30000)
