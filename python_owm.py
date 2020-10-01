75be4e112bda3586c08d02047474aab76b862fd6
Iglek Kuat <dizinnes228@gmail.com>
2020-10-02 00:07:08 +0600

    feat(root): add code for getting weather wind

--- python_owm.py
+++ python_owm.py
@@ -9,20 +9,7 @@ dotenv.load_dotenv()
 
 owm = OWM(os.getenv("WEATHER_API_KEY"))
 mgr = owm.weather_manager()
+one_call = mgr.one_call(lat=52.5244, lon=13.4105)
 
-
-# Search for current weather in London (Great Britain) and get details
-observation = mgr.weather_at_place('London,GB')
-w = observation.weather
-
-w.detailed_status()         # 'clouds'
-w.wind()                  # {'speed': 4.6, 'deg': 330}
-w.humidity()        # 87
-w.temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
-w.rain()                    # {}
-w.heat_index()              # None
-w.clouds()                  # 75
-
-# Will it be clear tomorrow at this time in Milan (Italy) ?
-forecast = mgr.forecast_at_place('Milan,IT', 'daily')
-answer = forecast.will_be_clear_at(timestamps.tomorrow())
+a = one_call.forecast_hourly[3].wind().get('speed', 0)
+print(a)
