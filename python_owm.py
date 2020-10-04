ae827470e80b833cc8cae38d0f0680c91c818a75
Iglek Kuat <dizinnes228@gmail.com>
2020-10-04 12:48:52 +0600

    refactor(weather): change and fix dict in forecast function

--- python_owm.py
+++ python_owm.py
@@ -12,7 +12,6 @@ dotenv.load_dotenv()
 owm = OWM(os.getenv("WEATHER_API_KEY"))
 mgr = owm.weather_manager()
 one_call = mgr.one_call(lat=52.5244, lon=13.4105)
-print()
 
 
 def current_weather():
@@ -32,19 +31,19 @@ def forecast_daily() -> List:
     day = 1
     for every_weather in range(0, 7):
         weather_dict = {
-            f'Weather information for day {day} ': {'Clouds': one_call.forecast_daily[every_weather].clouds,
-                                                    'Humidity': one_call.forecast_daily[every_weather].humidity,
-                                                    'Status': one_call.forecast_daily[every_weather].status,
-                                                    'Detailed status': one_call.forecast_daily[
-                                                        every_weather].detailed_status,
-                                                    'Visibility distance': one_call.forecast_daily[
-                                                        every_weather].visibility_distance,
-                                                    'Temperature': one_call.forecast_daily[
-                                                        every_weather].temperature().get("day",
-                                                                                         None),
-                                                    'Weather_icon_name': one_call.forecast_daily[
-                                                        every_weather].weather_icon_name
-                                                    }
+            'Clouds': one_call.forecast_daily[every_weather].clouds,
+            'Humidity': one_call.forecast_daily[every_weather].humidity,
+            'Status': one_call.forecast_daily[every_weather].status,
+            'Detailed status': one_call.forecast_daily[
+                every_weather].detailed_status,
+            'Visibility distance': one_call.forecast_daily[
+                every_weather].visibility_distance,
+            'Temperature': one_call.forecast_daily[
+                every_weather].temperature().get("day",
+                                                 None),
+            'Weather_icon_name': one_call.forecast_daily[
+                every_weather].weather_icon_name
+
         }
         day += 1
         weather.append(weather_dict)
@@ -53,4 +52,4 @@ def forecast_daily() -> List:
 
 
 for print_ in forecast_daily():
-    print(print_)
+    print(print_['Clouds'])
