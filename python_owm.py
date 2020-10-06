2581205ee3b0cd9713f8dcc2585b14f04e9bfee5
Iglek Kuat <dizinnes228@gmail.com>
2020-10-06 23:04:52 +0600

    refactor(root) change variables name in file

--- python_owm.py
+++ python_owm.py
@@ -11,18 +11,18 @@ dotenv.load_dotenv()
 
 owm = OWM(os.getenv("WEATHER_API_KEY"))
 mgr = owm.weather_manager()
-one_call = mgr.one_call(lat=52.5244, lon=13.4105)
+one_call_api = mgr.one_call_api(lat=52.5244, lon=13.4105)
 
 
 def current_weather():
     return {
-        'Clouds': one_call.current.clouds,
-        'Humidity': one_call.current.humidity,
-        'Status': one_call.current.status,
-        'Detailed status': one_call.current.detailed_status,
-        'Visibility distance': one_call.current.visibility_distance,
-        'Temperature': one_call.current.temp,
-        'Weather_icon_name': one_call.current.weather_icon_name,
+        'Clouds': one_call_api.current.clouds,
+        'Humidity': one_call_api.current.humidity,
+        'Status': one_call_api.current.status,
+        'Detailed status': one_call_api.current.detailed_status,
+        'Visibility distance': one_call_api.current.visibility_distance,
+        'Temperature': one_call_api.current.temp,
+        'Weather_icon_name': one_call_api.current.weather_icon_name,
     }
 
 
@@ -31,17 +31,17 @@ def forecast_daily() -> List:
     day = 1
     for every_weather in range(0, 7):
         weather_dict = {
-            'Clouds': one_call.forecast_daily[every_weather].clouds,
-            'Humidity': one_call.forecast_daily[every_weather].humidity,
-            'Status': one_call.forecast_daily[every_weather].status,
-            'Detailed status': one_call.forecast_daily[
+            'Clouds': one_call_api.forecast_daily[every_weather].clouds,
+            'Humidity': one_call_api.forecast_daily[every_weather].humidity,
+            'Status': one_call_api.forecast_daily[every_weather].status,
+            'Detailed status': one_call_api.forecast_daily[
                 every_weather].detailed_status,
-            'Visibility distance': one_call.forecast_daily[
+            'Visibility distance': one_call_api.forecast_daily[
                 every_weather].visibility_distance,
-            'Temperature': one_call.forecast_daily[
+            'Temperature': one_call_api.forecast_daily[
                 every_weather].temperature().get("day",
                                                  None),
-            'Weather icon name': one_call.forecast_daily[
+            'Weather icon name': one_call_api.forecast_daily[
                 every_weather].weather_icon_name
 
         }
@@ -52,17 +52,16 @@ def forecast_daily() -> List:
 
 
 def output_weather():
-    for print_ in forecast_daily():
+    for weekly_weather in forecast_daily():
         print('-------------------------------------------------------------')
-        print('Clouds: ', print_['Clouds'])
-        print('Humidity: ', print_['Humidity'])
-        print('Status: ', print_['Status'])
-        print('Detailed status: ', print_['Detailed status'])
-        print('Visibility distance: ', print_['Visibility distance'])
-        print('Temperature: ', print_['Temperature'])
-        print('Weather icon name: ', print_['Weather icon name'])
+        print('Clouds: ', weekly_weather['Clouds'])
+        print('Humidity: ', weekly_weather['Humidity'])
+        print('Status: ', weekly_weather['Status'])
+        print('Detailed status: ', weekly_weather['Detailed status'])
+        print('Visibility distance: ', weekly_weather['Visibility distance'])
+        print('Temperature: ', weekly_weather['Temperature'])
+        print('Weather icon name: ', weekly_weather['Weather icon name'])
         print('-------------------------------------------------------------')
-
 
 
 output_weather()
